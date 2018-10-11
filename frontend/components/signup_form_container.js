2381198236248469ed2e15b9264e17d6242f9264
Grace Park <parkg748@newschool.edu>
2018-10-10 17:03:52 -0700

    Add back the lost css

---
+++ frontend/components/signup_form_container.js
@@ -0,0 +1,19 @@
+import {connect} from 'react-router-dom';
+import SignupForm from './signup_form';
+import {receiveErrors, clearErrors} from '../actions/session_actions';
+
+const mapStateToProps = state => {
+  return {
+    signupForm: {name: '', email: '', reemail: '', password: '', repassword: '', emailError: 'error-email-msg'},
+    errors: state.errors
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    receiveErrors: errors => dispatch(receiveErrors(errors)),
+    clearErrors: () => dispatch(clearErrors())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignupForm);
