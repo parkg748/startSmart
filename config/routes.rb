0fc2fd3de30075c0a98f6ecc5935d7a97bd60d0a
Grace Park <parkg748@newschool.edu>
2018-11-14 12:41:24 -0800

    fixing image uploading

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:show, :create, :edit] do
-      resources :projects, only: [:index, :create, :show, :edit, :delete] do
+    resources :users, only: [:index, :show, :create, :edit] do
+      resources :projects, only: [:index, :create, :show, :edit, :destroy] do
         resources :rewards, only: [:index, :create, :show] do
           resources :items, only: [:index, :create, :show]
         end
       end
     end
-    resources :projects, only: [:index, :update]
+    resources :users, only: [:update]
+    resources :projects, only: [:index, :update, :show, :destroy]
     resource :session, only: [:create, :destroy]
     resources :categories, only: [:show, :index] do
       resources :projects, only: [:index]
