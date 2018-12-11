85d93abfcf4602de0c95936bb5947522036bdbf0
Grace Park <parkg748@newschool.edu>
2018-12-10 21:45:29 -0800

    add react calendar and add reward box

--- frontend/components/basics_form.jsx
+++ frontend/components/basics_form.jsx
@@ -1,37 +1,23 @@
 import React from 'react';
-import 'react-dates/initialize';
-import { DateRangePicker, SingleDatePicker } from 'react-dates';
 
 class BasicsForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: null,
-      focused: false
-    };
-    this.onDateChange = this.onDateChange.bind(this);
-    this.onFocusChange = this.onFocusChange.bind(this);
-  }
-
-  onDateChange(date) {
-    this.setState({date});
-  }
-
-  onFocusChange(focused) {
-    this.setState({focused});
-  }
-
-  render() {
-    return (
-      <SingleDatePicker
-    id="date_input"
-    date={this.state.date}
-    focused={this.state.focused}
-    onDateChange={this.onDateChange}
-    onFocusChange={this.onFocusChange}
-/>
-    );
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     date: new Date(),
+  //   };
+  //   this.onChange = this.onChange.bind(this);
+  // }
+  //
+  // onChange(date) {
+  //   this.setState({ date });
+  // }
+  //
+  // render() {
+  //   return (
+  //     <Calendar onChange={this.onChange} value={this.state.date} />
+  //   );
+  // }
 }
 
 export default BasicsForm;
