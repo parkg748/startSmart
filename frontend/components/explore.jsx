c69c3d74433d10e54f4c87a69ddd0e6c8be635a6
Grace Park <parkg748@newschool.edu>
2018-12-11 18:01:43 -0800

    add links to explore page

--- frontend/components/explore.jsx
+++ frontend/components/explore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class Explore extends React.Component {
   constructor(props) {
@@ -14,14 +15,14 @@ class Explore extends React.Component {
         <div className='explore-collections-tab-down'>
           <div className='explore-collections-tab-down-inner'>
             <ul>
-              <li>Recommended For You</li>
-              <li>Projects We Love</li>
-              <li>Saved Projects</li>
-              <li>Trending</li>
-              <li>Nearly Funded</li>
-              <li>Just Launched</li>
-              <li>Backed By People You Follow</li>
-              <li>Everything</li>
+              <li><Link to='/recommendations'>Recommended For You</Link></li>
+              <li><Link to='/'>Projects We Love</Link></li>
+              <li><Link to='/'>Saved Projects</Link></li>
+              <li><Link to='/'>Trending</Link></li>
+              <li><Link to='/'>Nearly Funded</Link></li>
+              <li><Link to='/'>Just Launched</Link></li>
+              <li><Link to='/'>Backed By People You Follow</Link></li>
+              <li><Link to='/'>Everything</Link></li>
             </ul>
           </div>
         </div>
@@ -31,14 +32,14 @@ class Explore extends React.Component {
         <div className='explore-collections-tab-down'>
           <div className='explore-collections-tab-down-inner'>
             <ul>
-              <li>Arts</li>
-              <li>Comics & Illustration</li>
-              <li>Design & Tech</li>
-              <li>Film</li>
-              <li>Food & Craft</li>
-              <li>Games</li>
-              <li>Music</li>
-              <li>Publishing</li>
+              <li><Link to='/'>Arts</Link></li>
+              <li><Link to='/'>Comics & Illustration</Link></li>
+              <li><Link to='/'>Design & Tech</Link></li>
+              <li><Link to='/'>Film</Link></li>
+              <li><Link to='/'>Food & Craft</Link></li>
+              <li><Link to='/'>Games</Link></li>
+              <li><Link to='/'>Music</Link></li>
+              <li><Link to='/'>Publishing</Link></li>
             </ul>
           </div>
         </div>
@@ -48,21 +49,21 @@ class Explore extends React.Component {
         <div className='explore-collections-tab-down'>
           <div className='explore-collections-tab-down-inner'>
             <ul>
-              <li>Art</li>
-              <li>Comics</li>
-              <li>Crafts</li>
-              <li>Dance</li>
-              <li>Design</li>
-              <li>Fashion</li>
-              <li>Film & Video</li>
-              <li>Food</li>
-              <li>Games</li>
-              <li>Journalism</li>
-              <li>Music</li>
-              <li>Photography</li>
-              <li>Publishing</li>
-              <li>Technology</li>
-              <li>Theater</li>
+              <li><Link to='/'>Art</Link></li>
+              <li><Link to='/'>Comics</Link></li>
+              <li><Link to='/'>Crafts</Link></li>
+              <li><Link to='/'>Dance</Link></li>
+              <li><Link to='/'>Design</Link></li>
+              <li><Link to='/'>Fashion</Link></li>
+              <li><Link to='/'>Film & Video</Link></li>
+              <li><Link to='/'>Food</Link></li>
+              <li><Link to='/'>Games</Link></li>
+              <li><Link to='/'>Journalism</Link></li>
+              <li><Link to='/'>Music</Link></li>
+              <li><Link to='/'>Photography</Link></li>
+              <li><Link to='/'>Publishing</Link></li>
+              <li><Link to='/'>Technology</Link></li>
+              <li><Link to='/'>Theater</Link></li>
             </ul>
           </div>
         </div>
@@ -72,18 +73,18 @@ class Explore extends React.Component {
         <div className='explore-collections-tab-down'>
           <div className='explore-collections-tab-down-inner'>
             <ul>
-              <li>Accessible Art</li>
-              <li>Bikes</li>
-              <li>DIY</li>
-              <li>Go Green</li>
-              <li>Just for Kids</li>
-              <li>LGBTQIA+</li>
-              <li>Public Benefit</li>
-              <li>Quickstarter</li>
-              <li>Robots</li>
-              <li>RPGs</li>
-              <li>Sci-Fi and Fantasy</li>
-              <li>STEM</li>
+              <li><Link to=''>Accessible Art</Link></li>
+              <li><Link to=''>Bikes</Link></li>
+              <li><Link to=''>DIY</Link></li>
+              <li><Link to=''>Go Green</Link></li>
+              <li><Link to=''>Just for Kids</Link></li>
+              <li><Link to=''>LGBTQIA+</Link></li>
+              <li><Link to=''>Public Benefit</Link></li>
+              <li><Link to=''>Quickstarter</Link></li>
+              <li><Link to=''>Robots</Link></li>
+              <li><Link to=''>RPGs</Link></li>
+              <li><Link to=''>Sci-Fi and Fantasy</Link></li>
+              <li><Link to=''>STEM</Link></li>
             </ul>
           </div>
         </div>
