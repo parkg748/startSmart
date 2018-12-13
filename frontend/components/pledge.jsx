dc3cfad994c352576432eb6323a281b47eb11cc0
Grace Park <parkg748@newschool.edu>
2018-12-12 17:17:34 -0800

    add project title and author to pledge page

--- frontend/components/pledge.jsx
+++ frontend/components/pledge.jsx
@@ -6,6 +6,11 @@ class Pledge extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    this.props.fetchProject(this.props.match.params.userId, this.props.match.params.projectId);
+    this.props.fetchUser(this.props.match.params.userId);
+  }
+
   render() {
     return (
       <div>
@@ -13,13 +18,22 @@ class Pledge extends React.Component {
           <Link to='/'><img className='logo-pledge' src='https://i.imgur.com/YuU5VqC.jpg' /></Link>
         </nav>
         <div className='pledge-title'>
-          <a>Project Title</a>
-          <span>by Project Author</span>
+          <a>{Object.values(this.props.project).length === 0 ? '' : Object.values(this.props.project)[0].title}</a>
+          <span>by {Object.values(this.props.user).length === 1 ? '' : Object.values(this.props.user)[0].name}</span>
         </div>
         <div className='support-this-project'>
           <div className='support-this-project-inner'>
             <div className='support-this-project-left'>
               <h2>Support this project</h2>
+              <div className='make-pledge-without-reward'>
+                <input type='radio' />
+                <div className='make-pledge-without-reward-content'>
+                  Make a pledge without a reward
+                </div>
+              </div>
+              <ul>
+                <li></li>
+              </ul>
             </div>
             <div className='support-this-project-right'>
             </div>
--- frontend/components/pledge_container.js
+++ frontend/components/pledge_container.js
@@ -1,15 +1,19 @@
 import { connect } from 'react-redux';
 import Pledge from './pledge';
+import { fetchProject } from '../actions/project_actions';
+import { fetchUser } from '../actions/session_actions';
 
 const mapStateToProps = state => {
   return {
-
+    project: state.entities.project,
+    user: state.entities.users
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-
+    fetchProject: (project, id) => dispatch(fetchProject(project, id)),
+    fetchUser: userId => dispatch(fetchUser(userId))
   };
 };
 
