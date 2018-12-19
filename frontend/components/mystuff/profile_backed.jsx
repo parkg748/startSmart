92dc3b325b509bd50268eb89a5a433e68bf1537c
Grace Park <parkg748@newschool.edu>
2018-12-19 14:30:46 -0800

    finish layout for created profie section

--- frontend/components/mystuff/profile_backed.jsx
+++ frontend/components/mystuff/profile_backed.jsx
@@ -2,6 +2,164 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function ProfileBacked({}) {
+  let temp = (<div className='profile-backed-show-container'>
+    <div className='profile-backed-show-inner'>
+      <ul>
+        <li className='profile-backed-show-inner-left'>
+          <div className='profile-backed-show-inner-left-inner'>
+            <img />
+            <div className='profile-backed-show-inner-bottom'>
+              <div className='profile-backed-show-inner-bottom-inner'>
+                <div className='profile-backed-show-inner-bottom-inner-inner'>
+                  <h3>KORE TEK BELT</h3>
+                  <div className='profile-backed-show-inner-bottom-description'>PERFECT FIT. Features Titanium Buckles, Micro Adjustability, Full-Grain Leather belt, Flex-Core™ Center.</div>
+                </div>
+              </div>
+              <div className='profile-backed-show-inner-bottom-bottom'>
+                <div className='profile-backed-show-inner-bottom-bottom-inner'>
+                  <div className='profile-backed-show-inner-bottom-bottom-inner-inner'>
+                    <div className='profile-backed-show-inner-bottom-bottom-inner-inner-inner'>
+                      <img />
+                      <div className='profile-backed-show-inner-bottom-author'>
+                        by <a>Karl Kozak</a> and 1,517 backers
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className='profile-backed-show-inner-center'>
+          <div className='profile-backed-show-inner-left-inner'>
+            <img />
+            <div className='profile-backed-show-inner-bottom'>
+              <div className='profile-backed-show-inner-bottom-inner'>
+                <div className='profile-backed-show-inner-bottom-inner-inner'>
+                  <h3>KORE TEK BELT</h3>
+                  <div className='profile-backed-show-inner-bottom-description'>PERFECT FIT. Features Titanium Buckles, Micro Adjustability, Full-Grain Leather belt, Flex-Core™ Center.</div>
+                </div>
+              </div>
+              <div className='profile-backed-show-inner-bottom-bottom'>
+                <div className='profile-backed-show-inner-bottom-bottom-inner'>
+                  <div className='profile-backed-show-inner-bottom-bottom-inner-inner'>
+                    <div className='profile-backed-show-inner-bottom-bottom-inner-inner-inner'>
+                      <img />
+                      <div className='profile-backed-show-inner-bottom-author'>
+                        by <a>Karl Kozak</a> and 1,517 backers
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className='profile-backed-show-inner-center'>
+          <div className='profile-backed-show-inner-left-inner'>
+            <img />
+            <div className='profile-backed-show-inner-bottom'>
+              <div className='profile-backed-show-inner-bottom-inner'>
+                <div className='profile-backed-show-inner-bottom-inner-inner'>
+                  <h3>KORE TEK BELT</h3>
+                  <div className='profile-backed-show-inner-bottom-description'>PERFECT FIT. Features Titanium Buckles, Micro Adjustability, Full-Grain Leather belt, Flex-Core™ Center.</div>
+                </div>
+              </div>
+              <div className='profile-backed-show-inner-bottom-bottom'>
+                <div className='profile-backed-show-inner-bottom-bottom-inner'>
+                  <div className='profile-backed-show-inner-bottom-bottom-inner-inner'>
+                    <div className='profile-backed-show-inner-bottom-bottom-inner-inner-inner'>
+                      <img />
+                      <div className='profile-backed-show-inner-bottom-author'>
+                        by <a>Karl Kozak</a> and 1,517 backers
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <ul>
+        <li className='profile-backed-show-inner-left'>
+          <div className='profile-backed-show-inner-left-inner'>
+            <img />
+            <div className='profile-backed-show-inner-bottom'>
+              <div className='profile-backed-show-inner-bottom-inner'>
+                <div className='profile-backed-show-inner-bottom-inner-inner'>
+                  <h3>KORE TEK BELT</h3>
+                  <div className='profile-backed-show-inner-bottom-description'>PERFECT FIT. Features Titanium Buckles, Micro Adjustability, Full-Grain Leather belt, Flex-Core™ Center.</div>
+                </div>
+              </div>
+              <div className='profile-backed-show-inner-bottom-bottom'>
+                <div className='profile-backed-show-inner-bottom-bottom-inner'>
+                  <div className='profile-backed-show-inner-bottom-bottom-inner-inner'>
+                    <div className='profile-backed-show-inner-bottom-bottom-inner-inner-inner'>
+                      <img />
+                      <div className='profile-backed-show-inner-bottom-author'>
+                        by <a>Karl Kozak</a> and 1,517 backers
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className='profile-backed-show-inner-center'>
+          <div className='profile-backed-show-inner-left-inner'>
+            <img />
+            <div className='profile-backed-show-inner-bottom'>
+              <div className='profile-backed-show-inner-bottom-inner'>
+                <div className='profile-backed-show-inner-bottom-inner-inner'>
+                  <h3>KORE TEK BELT</h3>
+                  <div className='profile-backed-show-inner-bottom-description'>PERFECT FIT. Features Titanium Buckles, Micro Adjustability, Full-Grain Leather belt, Flex-Core™ Center.</div>
+                </div>
+              </div>
+              <div className='profile-backed-show-inner-bottom-bottom'>
+                <div className='profile-backed-show-inner-bottom-bottom-inner'>
+                  <div className='profile-backed-show-inner-bottom-bottom-inner-inner'>
+                    <div className='profile-backed-show-inner-bottom-bottom-inner-inner-inner'>
+                      <img />
+                      <div className='profile-backed-show-inner-bottom-author'>
+                        by <a>Karl Kozak</a> and 1,517 backers
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className='profile-backed-show-inner-center'>
+          <div className='profile-backed-show-inner-left-inner'>
+            <img />
+            <div className='profile-backed-show-inner-bottom'>
+              <div className='profile-backed-show-inner-bottom-inner'>
+                <div className='profile-backed-show-inner-bottom-inner-inner'>
+                  <h3>KORE TEK BELT</h3>
+                  <div className='profile-backed-show-inner-bottom-description'>PERFECT FIT. Features Titanium Buckles, Micro Adjustability, Full-Grain Leather belt, Flex-Core™ Center.</div>
+                </div>
+              </div>
+              <div className='profile-backed-show-inner-bottom-bottom'>
+                <div className='profile-backed-show-inner-bottom-bottom-inner'>
+                  <div className='profile-backed-show-inner-bottom-bottom-inner-inner'>
+                    <div className='profile-backed-show-inner-bottom-bottom-inner-inner-inner'>
+                      <img />
+                      <div className='profile-backed-show-inner-bottom-author'>
+                        by <a>Karl Kozak</a> and 1,517 backers
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>);
   return (
     <div className='profile-container-footer'>
       <div className='profile-container-footer-one'>
---
+++ frontend/components/mystuff/profile_created.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function ProfileCreated({}) {
+  return (
+    <div className='profile-created-container'>
+      <ul>
+        <div className='profile-created-inner'>
+          <div className='profile-created-section'>
+            <div className='profile-created-section-inner'>
+              <img />
+              <div className='profile-created-section-content'>
+                <div className='profile-created-section-content-header'>
+                  <div className='profile-created-section-content-header-inner'>
+                    <h1>Silk Threads: Novellas by Midori Laura Antoniou Cecilia Tan</h1>
+                    <p>Three interrelated erotic novellas of fantastical Japan--historical past, magical present, cyberpunk future --by three master creators.</p>
+                  </div>
+                </div>
+                <div className='profile-created-section-author'>
+                  <div className='profile-created-section-author-inner'>
+                    <div className='profile-created-section-author-inner-inner'>
+                      <img />
+                      <div className='profile-created-section-author-inner-inner-inner'>
+                        by <a>Cecilia Tan</a> and 576 backers
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='profile-created-section'>
+            <div className='profile-created-section-inner'>
+              <img />
+              <div className='profile-created-section-content'>
+                <div className='profile-created-section-content-header'>
+                  <div className='profile-created-section-content-header-inner'>
+                    <h1>Silk Threads: Novellas by Midori Laura Antoniou Cecilia Tan</h1>
+                    <p>Three interrelated erotic novellas of fantastical Japan--historical past, magical present, cyberpunk future --by three master creators.</p>
+                  </div>
+                </div>
+                <div className='profile-created-section-author'>
+                  <div className='profile-created-section-author-inner'>
+                    <div className='profile-created-section-author-inner-inner'>
+                      <img />
+                      <div className='profile-created-section-author-inner-inner-inner'>
+                        by <a>Cecilia Tan</a> and 576 backers
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ul>
+    </div>
+  );
+}
+
+export default ProfileCreated;
